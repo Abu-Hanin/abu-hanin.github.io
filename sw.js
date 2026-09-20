3f8ee565f751ef735e2b0203e00b8e772faba5b9
walid <swlyd3393@gmail.com>
2026-09-21 00:10:26 +0300

    Update CACHE_NAME to 'blotrade-v30'

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
-const CACHE_NAME = 'blotrade-v28';
+const CACHE_NAME = 'blotrade-v30';
 const urlsToCache = [
   './',
   './index.html',
